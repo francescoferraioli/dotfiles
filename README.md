f6e13c68dc9e5ec9216443c231061ba8aa2ad9b6
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2019-04-05 16:04:28 +1000

    Move instructions in `README` into the `once` script

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+ZSH set up is following this article
 https://medium.com/@Clovis_app/configuration-of-a-beautiful-efficient-terminal-and-prompt-on-osx-in-7-minutes-827c29391961
 
 HammerSpoon
@@ -14,37 +15,7 @@ require('keyboard.microphone')
 require('keyboard.windows')
 ```
 
-sudo chsh -s /bin/zsh
-
-Fresh?
-
-- https://github.com/junegunn/fzf
-- https://github.com/BurntSushi/ripgrep
+# Installation
 ```
-brew install ripgrep
-```
-- https://github.com/sharkdp/fd
-- https://github.com/sharkdp/bat
-```
-brew install bat
-```
-- https://github.com/stedolan/jq
-```
-brew install jq
-```
-- https://github.com/so-fancy/diff-so-fancy
-```
-npm install -g diff-so-fancy
-```
-- https://github.com/jwilm/alacritty
-```
-brew cask install alacritty
-```
-- https://github.com/tmux/tmux
-```
-brew install tmux
-```
-- https://github.com/jamesottaway/tmux-up
-```
-curl -L https://git.io/tmux-up -o /usr/local/bin/tmux-up
+./once.sh
 ```
--- once.sh
+++ once.sh
@@ -1,54 +1,34 @@
-# Install base16 shell
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-
 # Install Homebrew /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew install git
-brew cask install iterm2
-brew install zsh zsh-completions
-brew install fd
-brew install rg
-brew install fzf 
-brew cask install alacritty
-brew install tmux
-brew install bat
-
-#### Git
-
-brew install diff-so-fancy
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
-git config --global color.ui true
-git config --global color.diff-highlight.oldNormal    "red bold"
-git config --global color.diff-highlight.oldHighlight "red bold 52"
-git config --global color.diff-highlight.newNormal    "green bold"
-git config --global color.diff-highlight.newHighlight "green bold 22"
-git config --global color.diff.meta       "yellow"
-git config --global color.diff.frag       "magenta bold"
-git config --global color.diff.commit     "yellow bold"
-git config --global color.diff.old        "red bold"
-git config --global color.diff.new        "green bold"
-git config --global color.diff.whitespace "red reverse"
-
-git config --global core.editor vim
-
-#####
 
 defaults write com.apple.finder AppleShowAllFiles YES
 
+brew install git
+brew install diff-so-fancy
+
+brew cask install iterm2
+brew cask install alacritty
+
+brew install fd
+brew install rg
+brew install fzf 
+brew install bat
+brew install jq
+
+brew install tmux
+curl -L https://git.io/tmux-up -o /usr/local/bin/tmux-up
+
+brew install zsh zsh-completions
+brew install zsh-syntax-highlighting
+
 # Install "Oh my ZSH"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo chsh -s /bin/zsh
 
 # Install "Powerlevel9k Zsh Theme"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-brew install zsh-syntax-highlighting
-
-sudo rm -rf ~/Documents
-sudo rm -rf ~/Music
-sudo rm -rf ~/Movies
-sudo rm -rf ~/Public
-sudo rm -rf ~/Pictures
-mkdir -p ~/Code
+# Install base16 shell
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
 curl https://nixos.org/nix/install | sh
 nix-channel --remove nixpkgs
