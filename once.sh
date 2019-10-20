baebafda16eb81731886dd93a4f7b27f6313507b
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2019-10-20 18:58:07 +1000

    Update `once` script to call `Brewfile`

--- once.sh
+++ once.sh
@@ -2,34 +2,26 @@
 
 defaults write com.apple.finder AppleShowAllFiles YES
 
-brew install git
-brew install diff-so-fancy
+# BREW
+brew update
+brew upgrade
+brew doctor
+brew bundle
 
-brew cask install iterm2
-brew cask install alacritty
-
-brew install fd
-brew install rg
-brew install fzf 
-brew install bat
-brew install jq
-
-brew install tmux
+# TMUX UP
 curl -L https://git.io/tmux-up -o /usr/local/bin/tmux-up
 
-brew install zsh zsh-completions
-brew install zsh-syntax-highlighting
-
-# Install "Oh my ZSH"
+# OH MY ZSH
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo chsh -s /bin/zsh
 
-# Install "Powerlevel9k Zsh Theme"
+# POWERLEVEL9K ZSH THEME
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-# Install base16 shell
+# BASE16 SHELL
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
+# NIX
 curl https://nixos.org/nix/install | sh
 nix-channel --remove nixpkgs
 nix-channel --add https://nixos.org/channels/nixos-18.03 nixpkgs
