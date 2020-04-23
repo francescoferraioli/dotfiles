fa85573f3ad4eae0eb485d42068165c61df27f00
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2020-04-24 09:40:15 +1000

    Remove manually installing oh-my-zsh and powerlevel9k
    We are installing them via nix/brew now

--- once.sh
+++ once.sh
@@ -12,13 +12,6 @@ brew bundle
 # TMUX UP
 curl -L https://git.io/tmux-up -o /usr/local/bin/tmux-up
 
-# OH MY ZSH
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo chsh -s /bin/zsh
-
-# POWERLEVEL9K ZSH THEME
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
 # BASE16 SHELL
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
