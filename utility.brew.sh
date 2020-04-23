29f0f3866e85eb052e8002983fe45d95b56b2740
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2020-04-23 15:55:33 +1000

    Seperate Brewfile into brew/cask files

---
+++ utility.brew.sh
@@ -0,0 +1,21 @@
+# GIT
+brew install git
+brew install diff-so-fancy
+
+# UTILITY
+brew install fd
+brew install rg
+brew install fzf 
+brew install bat
+brew install jq
+
+# NODE
+brew install node
+
+# TMUX
+brew install tmux
+
+# ZSH
+brew install zsh
+brew install zsh-completions
+brew install zsh-syntax-highlighting
---
+++ utility.cask.sh
@@ -0,0 +1,26 @@
+#CASK
+brew cask install alacritty
+brew cask install alfred
+brew cask install visual-studio-code
+brew cask install fork
+brew cask install postman
+brew cask install firefox
+brew cask install google-chrome
+brew cask install chromium
+brew cask install hammerspoon
+brew cask install iterm2
+brew cask install lastpass
+brew cask install 1password
+brew cask install slack
+brew cask install spotify
+
+# FONTS
+brew tap homebrew/cask-fonts
+brew cask install font-meslo-for-powerline
+brew cask install font-anonymous-pro
+brew cask install font-inconsolata
+brew cask install font-roboto
+brew cask install font-source-code-pro
+brew cask install font-source-sans-pro
+brew cask install font-terminus
+brew cask install font-ubuntu
