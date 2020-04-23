f405272ceb6abfbf6bc5851c445dd9681547fcc8
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2020-04-23 15:55:55 +1000

    Add nix equivalent of brew file

---
+++ utility.nix.sh
@@ -0,0 +1,21 @@
+# GIT
+nix-env -i git
+nix-env -i diff-so-fancy
+
+# UTILITY
+nix-env -i fd
+nix-env -i ripgrep
+nix-env -i fzf 
+nix-env -i bat
+nix-env -i jq
+
+# NODE
+nix-env -i nodejs
+
+# TMUX
+nix-env -i tmux
+
+# ZSH
+nix-env -i zsh
+nix-env -i zsh-completions
+nix-env -i zsh-syntax-highlighting
