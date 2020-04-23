875106c1d486428e921eb94a951777a58fb57fed
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2020-04-24 09:53:24 +1000

    Add `tmuxspace` to once.sh

--- once.sh
+++ once.sh
@@ -12,6 +12,9 @@ brew bundle
 # TMUX UP
 curl -L https://git.io/tmux-up -o /usr/local/bin/tmux-up
 
+# TMUXSPACE
+curl https://raw.githubusercontent.com/francescoferraioli/tmuxspace/master/tmuxspace -o /usr/local/bin/tmuxspace
+
 # BASE16 SHELL
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
