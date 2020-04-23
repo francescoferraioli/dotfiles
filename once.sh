8cafc5620be5e98449d06bd585d2c72eaad90cfb
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2020-04-24 09:53:56 +1000

    Make `tmux-up` and `tmuxspace` executable

--- once.sh
+++ once.sh
@@ -11,9 +11,11 @@ brew bundle
 
 # TMUX UP
 curl -L https://git.io/tmux-up -o /usr/local/bin/tmux-up
+chmod u+x /usr/local/bin/tmux-up
 
 # TMUXSPACE
 curl https://raw.githubusercontent.com/francescoferraioli/tmuxspace/master/tmuxspace -o /usr/local/bin/tmuxspace
+chmod u+x /usr/local/bin/tmuxspace
 
 # BASE16 SHELL
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
