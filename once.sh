dfe308fc61d032cf439e14b7d3aaec4bdef68693
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2020-04-24 15:04:16 +1000

    Make headers more like headers in once

--- once.sh
+++ once.sh
@@ -1,26 +1,40 @@
+###############
 # INSTALLERS
+###############
 # nix
 # curl -L https://nixos.org/nix/install | sh
 # brew
 # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+###############
 # APPLE SETTINGS
+###############
 defaults write com.apple.finder AppleShowAllFiles YES
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+###############
 # SCRIPTS
+###############
 git clone https://github.com/francescoferraioli/scripts.git ~/scripts
 
+###############
 # TMUX UP
+###############
 curl -L https://git.io/tmux-up -o /usr/local/bin/tmux-up
 chmod u+x /usr/local/bin/tmux-up
 
+###############
 # TMUXSPACE
+###############
 curl https://raw.githubusercontent.com/francescoferraioli/tmuxspace/master/tmuxspace -o /usr/local/bin/tmuxspace
 chmod u+x /usr/local/bin/tmuxspace
 
+###############
 # BASE16 SHELL
+###############
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
+###############
 # DOTFILES SYMLINKS
+###############
 ./create-links.sh
