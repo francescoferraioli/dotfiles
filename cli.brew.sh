df289c4b3e22104ab313d109220779bbd6abd10a
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-01-27 11:00:34 +1000

    Readd the cli tools by brew

---
+++ cli.brew.sh
@@ -0,0 +1,23 @@
+# GIT
+brew install git
+brew install git-open
+brew install diff-so-fancy
+
+# UTILITY
+brew install fd
+brew install rg
+brew install fzf 
+brew install bat
+brew install jq
+
+# NODE
+brew install node
+
+# TMUX
+brew install tmux
+
+# TERMINAL
+brew install zsh
+brew install zsh-completions
+brew install zsh-syntax-highlighting
+brew install starship
