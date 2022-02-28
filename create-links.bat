6882d4c052c0d6fe08b262a92b30bdbdabf8026f
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2022-02-28 12:29:21 +1000

    Add create-links.bat

---
+++ create-links.bat
@@ -0,0 +1,4 @@
+mklink /H .gitconfig dotfiles\git\gitconfig
+mklink /H .vimrc dotfiles\vim\vimrc
+mklink /H .vsvimrc dotfiles\vim\vsvimrc
+mklink /H .config\starship.toml dotfiles\starship\starship.toml
