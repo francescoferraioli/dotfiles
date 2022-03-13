fbd57819b0de530c90356d793fed366e1c216470
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2022-03-14 09:32:41 +1000

    Rename `.bat` to `.cmd`

--- create-links.bat
+++
@@ -1,8 +0,0 @@
-:: Run this from the root
-@echo off
-call .\dotfiles\create-link.bat .bashrc dotfiles\bash\bashrc
-call .\dotfiles\create-link.bat .inputrc dotfiles\bash\inputrc
-call .\dotfiles\create-link.bat .gitconfig dotfiles\git\gitconfig
-call .\dotfiles\create-link.bat .vimrc dotfiles\vim\vimrc
-call .\dotfiles\create-link.bat .vsvimrc dotfiles\vim\vsvimrc
-call .\dotfiles\create-link.bat .config\starship.toml dotfiles\starship\starship.toml
