71fbbb0c84c5a6a27c1e158194500e3976de8964
Frankie Ferraioli <francesco.ferraioli@outlook.com>
2026-03-09 14:05:34 +1000

    Add otter / cc setup for devbox

--- canva-devbox/setup.sh
+++ canva-devbox/setup.sh
@@ -2,6 +2,9 @@
 
 npm install -g diff-so-fancy
 
+otter claude-code mcp add otter otter mcp serve
+otter config mcp enable-all
+
 git clone https://github.com/francescoferraioli/scripts.git
 
 git clone https://github.com/francescoferraioli/ff.git
