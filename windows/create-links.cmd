1e50e0202a5689782d98f4e162bb475e1883d23d
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2026-03-11 22:45:49 +1000

    Add gitconfig gfm

--- windows/create-links.cmd
+++ windows/create-links.cmd
@@ -3,6 +3,7 @@ cd %HOMEPATH%
 call .\dotfiles\windows\create-link.cmd .bashrc dotfiles\bash\bashrc
 call .\dotfiles\windows\create-link.cmd .inputrc dotfiles\bash\inputrc
 call .\dotfiles\windows\create-link.cmd .gitconfig dotfiles\git\gitconfig
+call .\dotfiles\windows\create-link.cmd .gitconfig-gfm dotfiles\git\gitconfig-gfm
 call .\dotfiles\windows\create-link.cmd .vimrc dotfiles\vim\vimrc
 call .\dotfiles\windows\create-link.cmd .vsvimrc dotfiles\vim\vsvimrc
 call .\dotfiles\windows\create-link.cmd .config\starship.toml dotfiles\starship\starship.toml
