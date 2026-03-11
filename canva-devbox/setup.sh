807015f955825aba5f7712c1124a3f081014295a
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2026-03-11 10:48:39 +1000

    Install startship

--- canva-devbox/setup.sh
+++ canva-devbox/setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Install diff-so-fancy
 npm install -g diff-so-fancy
 
+# Install fonts and starship
+sudo apt install fonts-powerline
+curl -sS https://starship.rs/install.sh | sh
+
+# Setup otter mcp
 otter claude-code mcp add otter otter mcp serve
 otter config mcp enable-all
 
+# Clone my repos
 git clone https://github.com/francescoferraioli/scripts.git
 
 git clone https://github.com/francescoferraioli/ff.git
