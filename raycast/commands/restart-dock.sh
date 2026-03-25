32b36e718d288487e01e71b4a14a932b0f863352
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2026-03-25 15:49:10 +1000

    Add raycast commands

---
+++ raycast/commands/restart-dock.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Restart Dock
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author Francesco Ferraioli
+
+~/scripts/.ff/mac/restart-dock
+
