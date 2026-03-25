5ece9464c6ada0ecc06cb4e5b294f8b5d7bdad0c
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2026-03-26 09:57:24 +1000

    Add raycast script to restart logitech

---
+++ raycast/commands/restart-logitech.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Restart Logitech
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author Francesco Ferraioli
+
+~/scripts/.ff/mac/restart-logitech
