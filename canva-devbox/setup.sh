d28a2a9b56122ef496efdb5a9a77b6ec2a22f812
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2026-03-26 11:36:08 +1000

    Move otter setup to after clones and creating links

--- canva-devbox/setup.sh
+++ canva-devbox/setup.sh
@@ -8,7 +8,18 @@ npm install -g diff-so-fancy
 sudo apt install fonts-powerline
 curl -sS https://starship.rs/install.sh | sh
 
+# Clone my repos
+git clone https://github.com/francescoferraioli/scripts.git
+
+git clone https://github.com/francescoferraioli/ff.git
+
+git clone https://github.com/canvanauts/frankie-claude.git
+./frankie-claude/create-links.sh
+
 # Setup otter mcp
+# Do this after cloning and creating the links as it sets things up in
+# the correct place for otter claude code to work and for mcp to be
+# installed in the correct place.
 otter config mcp enable-all
 otter claude-code mcp add otter -- otter mcp serve
 
@@ -23,11 +34,3 @@ read -rs -p "Jira API token: " JIRA_TOKEN
 echo
 otter config mcp secret jira_api_token "$JIRA_TOKEN"
 unset JIRA_TOKEN
-
-# Clone my repos
-git clone https://github.com/francescoferraioli/scripts.git
-
-git clone https://github.com/francescoferraioli/ff.git
-
-git clone https://github.com/canvanauts/frankie-claude.git
-./frankie-claude/create-links.sh
