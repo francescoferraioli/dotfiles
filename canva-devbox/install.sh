2c20f4a8366bcdfdf84ae098a13530d5b014ed4d
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2026-04-01 09:53:23 +1000

    Split install setup and create links

---
+++ canva-devbox/install.sh
@@ -0,0 +1,6 @@
+# Install diff-so-fancy
+npm install -g diff-so-fancy
+
+# Install fonts and starship
+sudo apt install fonts-powerline
+curl -sS https://starship.rs/install.sh | sh
--- canva-devbox/setup.sh
+++ canva-devbox/setup.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-./dotfiles/canva-devbox/create-links.sh
-
-# Install diff-so-fancy
-npm install -g diff-so-fancy
-
-# Install fonts and starship
-sudo apt install fonts-powerline
-curl -sS https://starship.rs/install.sh | sh
 
 # Clone my repos
 git clone https://github.com/francescoferraioli/scripts.git
@@ -14,7 +6,6 @@ git clone https://github.com/francescoferraioli/scripts.git
 git clone https://github.com/francescoferraioli/ff.git
 
 git clone https://github.com/canvanauts/frankie-claude.git
-./frankie-claude/create-links.sh
 
 # Setup otter mcp
 # Do this after cloning and creating the links as it sets things up in
@@ -34,3 +25,6 @@ read -rs -p "Jira API token: " JIRA_TOKEN
 echo
 otter config mcp secret jira_api_token "$JIRA_TOKEN"
 unset JIRA_TOKEN
+
+./install.sh
+./create-links.sh
