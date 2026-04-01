ade787585aa270ede769474f1854be674aa7f845
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2026-04-01 16:15:25 +1000

    Add raycast command to do zoom_sync

---
+++ raycast/commands/download_zoom_notes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title My Script Command
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author Francesco Ferraioli
+
+cd ~/frankie-assistant-canva && ./zoom_sync.sh
