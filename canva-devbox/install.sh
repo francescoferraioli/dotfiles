5ad85b4ebb51a793b4a8c3e3fd9607b3c7797b98
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2026-05-19 11:33:40 +1000

    Install rg on devbox

--- canva-devbox/install.sh
+++ canva-devbox/install.sh
@@ -1,8 +1,10 @@
 # Install diff-so-fancy
 npm install -g diff-so-fancy
 
-# Install fonts and starship
+sudo apt update
 sudo apt install fonts-powerline
+sudo apt install ripgrep
+
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
 npm i -g https://github.com/canvanauts/realyze-git-pr-train.git
